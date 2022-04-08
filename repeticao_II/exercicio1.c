9bc7f4ca912c69eacb86cee38b212aac636ffa95
Doglas Rocha <79492860+DoglasRocha@users.noreply.github.com>
2022-04-07 22:17:35 -0300

    solve exercise 1

---
+++ repeticao_II/exercicio1.c
@@ -0,0 +1,24 @@
+/* Escreva um programa que leia números digitados pelo usuário até que este digite um valor negativo. Neste momento,
+o programa deverá encerrar a entrada de dados e apresentar o maior valor entre todos os digitados. */
+
+#include <stdio.h>
+
+int main(void)
+{
+    float numero_fornecido = 0, maior_numero = 0;
+
+    while (numero_fornecido >= 0)
+    {
+        printf("Por favor digite um numero positivo: ");
+        scanf("%f", &numero_fornecido);
+
+        if (numero_fornecido > maior_numero)
+            maior_numero = numero_fornecido;
+
+        printf("Caso queira sair do programa, digite um numero negativo.\n\n");
+    }
+
+    printf("O maior numero fornecido foi: %.2f\n\n", maior_numero);
+
+    return 0;
+}
---
+++ repeticao_II/exercicio2.c
@@ -0,0 +1,11 @@
+/* Leia um conjunto de valores inteiros (até que o -1000 seja digitado). Em seguida, mostre quantos destes valores
+digitados foram positivos. Na próxima linha, deve-se mostrar a média de todos os valores positivos digitados, com um
+dígito após o ponto decimal.
+Exemplo de Entrada Exemplo de Saída
+7 4 valores positivos
+-5 Media: 7.4
+6
+-3.4
+4.6
+12
+-1000 */
