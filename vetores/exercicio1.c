6830a4897e889ec7a510c5febbb7b3f1677ab0ca
Doglas Rocha <79492860+DoglasRocha@users.noreply.github.com>
2022-05-10 14:19:59 -0300

    solve exercise 1 vetores

--- vetores/exercicio1.c
+++ vetores/exercicio1.c
@@ -5,27 +5,8 @@ um novo valor. */
 #include <stdio.h>
 #define TAMANHO 5
 
-int insere_valor(int *vetor[], int valor, int posicao)
-{
-    int esta_dentro = 0;
-
-    for (int i = 0; i < TAMANHO; i++)
-    {
-        if (*vetor[i] == valor)
-        {
-                esta_dentro = 1;
-                break;
-        }
-    }
-
-    if (!esta_dentro)
-    {
-        *vetor[posicao] = valor;
-        return 1;
-    }
-
-    return 0;
-}
+int insere_valor(int vetor[], int valor, int posicao);
+void imprime_vetor(int vetor[]);
 
 int main(void)
 {
@@ -36,10 +17,49 @@ int main(void)
         printf("Por favor, digite o elemento da posicao %d do vetor de tamanho %d: ", posicao, TAMANHO);
         scanf("%d", &valor);
 
-        sucesso = insere_valor(&vetor, valor, posicao);
+        sucesso = insere_valor(vetor, valor, posicao);
 
         if (sucesso)
             posicao++;
         
+        else
+            printf("Elemento repetido!!\n");
+        
     } while (posicao < TAMANHO);
+
+    imprime_vetor(vetor);
+
+    return 0;
+}
+
+int insere_valor(int vetor[], int valor, int posicao)
+{
+    int esta_dentro = 0;
+
+    for (int i = 0; i < TAMANHO; i++)
+    {
+        if (vetor[i] == valor)
+        {
+                esta_dentro = 1;
+                break;
+        }
+    }
+
+    if (!esta_dentro)
+    {
+        vetor[posicao] = valor;
+        return 1;
+    }
+
+    return 0;
+}
+
+void imprime_vetor(int vetor[])
+{
+    printf("[");
+
+    for (int i = 0; i < TAMANHO; i++)
+        printf("%d,", vetor[i]);
+
+    printf("]\n");
 }
